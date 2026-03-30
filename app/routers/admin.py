4f6e162b8189137f3a117620230db427677a329c
Дима <dilshod.khaidkulov@gmail.com>
2026-03-30 22:46:05 +0300

    update

--- app/routers/admin.py
+++ app/routers/admin.py
@@ -7,8 +7,9 @@ from config import settings
 
 admin = Router()
 
+admin.filters.append(F.from_user.id.in_(settings.ADMIN_IDS))
 
-@admin.message_created(Command('admin'), lambda event: event.message.sender.user_id in settings.ADMIN_IDS)
+@admin.message_created(Command('admin'))
 async def cmd_admin(event: MessageCreated):
     await event.message.answer('Доступ к админ-панель разрещен!')
     
