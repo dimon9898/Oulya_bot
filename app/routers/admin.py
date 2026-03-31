9dcd93c83270f6201ecf2d63d2d650c74a1839b7
Дима <dilshod.khaidkulov@gmail.com>
2026-03-31 11:36:05 +0300

    update

--- app/routers/admin.py
+++ app/routers/admin.py
@@ -7,7 +7,7 @@ from config import settings
 
 admin = Router()
 
-@admin.message_created(Command('admin', prefix='/'))
+@admin.message_created(F.message.body.text == '/admin')
 async def cmd_admin(event: MessageCreated):
     if event.from_user.user_id in settings.ADMIN_IDS:
         await event.message.answer('Доступ к админ-панели разрешён!')
